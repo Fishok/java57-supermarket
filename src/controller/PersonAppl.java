a111360907b52c2c7bcecb50ad3394df3771b22a
Edel <edelisakson@gmail.com>
2024-11-14 20:10:44 +0200

    added setters

--- src/controller/PersonAppl.java
+++ src/controller/PersonAppl.java
@@ -14,5 +14,11 @@ public class PersonAppl {
         System.out.println("person1 hashCode: " + person1.hashCode());
         System.out.println("person3 hashCode: " + person3.hashCode());
         System.out.println("Are person1 and person3 equals? " + person1.equals(person3));
+        System.out.println("=======================================");
+        person2.setName("Alice");
+        System.out.println("person1 hashCode: " + person1.hashCode());
+        System.out.println("person2 hashCode: " + person2.hashCode());
+        System.out.println("Are person1 and person2 equals? " + person1.equals(person2));
+
     }
 }
